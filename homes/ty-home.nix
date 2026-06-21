28ff03453b81c19571d9a3002e2e5c792b7d7e8b
Ty <ogoxy.yt@gmail.com>
2026-06-21 13:10:26 -0400

    System auto-upgrade & lock refresh: 2026-06-21 13:10

--- homes/ty-home.nix
+++ homes/ty-home.nix
@@ -8,9 +8,13 @@
 
 {
   home.stateVersion = "26.11";
-
+  
   imports = [ ../home.nix ];
-
+  
+  home.sessionPath = [
+    "$HOME/.local/bin"
+  ];
+  
   home.file = {
     ".config/starship.toml".source = ../config/starship/starship.toml;
   };
