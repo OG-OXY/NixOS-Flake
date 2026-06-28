f1de9e9fe26cd916cdfbc62e6c9070a985231526
Ty <ogoxy.yt@gmail.com>
2026-06-28 13:07:30 -0400

    System auto-upgrade & lock refresh: 2026-06-28 13:07

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,21 +12,38 @@
   };
 
   outputs =
-    inputs@{
+    {
       self,
       nixpkgs,
+      nixpkgs-stable,
       home-manager,
       ...
-    }:
+    }@inputs:
     {
       nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs self; };
         modules = [
+          ./config.nix
           {
             nixpkgs.hostPlatform = "x86_64-linux";
             nixpkgs.config.allowUnfree = true;
+            nixpkgs.overlays = [
+              (
+                final: prev:
+                let
+                  stablePkgs = import nixpkgs-stable {
+                    inherit (prev) system;
+                    config = prev.config;
+                  };
+                in
+                {
+                  # List to lock to last version of nixpkgs-unstable.
+                  cantarell-fonts = stablePkgs.cantarell-fonts;
+
+                }
+              )
+            ];
           }
-          ./config.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
