d3c1c9f513a80e20cc7c5937e53e95ad91f33bbd
Ty <ogoxy.yt@gmail.com>
2026-07-07 12:58:02 -0400

    refixed shit.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,15 @@
-
+set -gx EDITOR nvim
+set -gx VISUAL nvim
 if status is-interactive
   set -g fish_greeting "Welcome to NixOS!"
-  set -g fish_handle_reflow 1  
-  
+  set -g fish_handle_reflow 1
+  alias ls ls -a
+  abbr -a cds cd ~/NixOS/nixos
+  abbr -a ga git add -A
+  abbr -a gc git commit -m '"'
+  abbr -a gp git push -u origin master
+  abbr -a gpf git push -u --force origin master
+  abbr -a yz yazi
   abbr -a nv nvim
   abbr -a snv sudoedit nvim
   abbr -a v vis
@@ -11,7 +18,6 @@ if status is-interactive
   abbr -a nrs sudo nixos-rebuild switch --flake .#nixos
   abbr -a nrsu sudo nixos-rebuild switch --upgrade --flake .#nixos
   abbr -a nb nix backup
-
   fastfetch
   direnv hook fish | source
 end
